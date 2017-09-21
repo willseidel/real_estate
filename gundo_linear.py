637bd194b5794c629587174305b3337b8fc4f5e1
willseidel <will.seidel@gmail.com>
2017-09-20 22:29:28 -0700

    refactoring to be based on more local data

--- gundo_linear.py
+++ gundo_linear.py
@@ -3,7 +3,7 @@
 #- single family homes
 #- north of grand
 #- south of walnut
-#- west of center
+#- west of main
 #= data structure
 #entry = [sale_price, #BR, #BA, built sqft, lot sqft, address]
 import numpy as np
@@ -11,23 +11,21 @@ from sklearn import linear_model
 
 sales = []
 
-sales.append([1175000,2,1,1294,7927,'830 maryland'])
-sales.append([1240000,3,1,1038,6660,'646 maryland'])
-sales.append([1099000,3,2,1600,6000,'602 lomita'])
-sales.append([1412500,6,4,2943,4948,'400 bungalow'])
-sales.append([850000,2,2,1120,2692,'815 penn'])
-sales.append([1866970,4,3,2845,5253,'619 eucalyptus'])
-sales.append([1040684,3,2,672,6714,'218 w oak'])
-sales.append([1130000,3,2,1026,4791,'811 concord'])
-sales.append([1300000,3,3,1520,7309,'300 w sycamore'])
-sales.append([1275000,2,1,1194,7152,'521 concord'])
-sales.append([1120000,4,1,1197,8232,'414 w oak'])
-sales.append([1400000,3,2,1564,6398,'432 loma vista'])
-sales.append([1229000,3,2,1528,4922,'606 w mariposa'])
-sales.append([910000,2,1,1236,3963,'631 loma vista'])
+sales.append([1300000,3,3,1520,7308,'300 w sycamore'])
+sales.append([1130000,3,2,1026,5018,'811 concord pl'])
 sales.append([1100000,2,1,948,8253,'650 w maple'])
-sales.append([1268500,4,3,1920,7016,'716 w oak'])
-sales.append([1381000,4,3,2919,5044,'734 redwood'])
+sales.append([1268500,4,3,1920,7017,'716 w oak'])
+sales.append([1255000,5,2,2599,5888,'728 loma vista'])
+sales.append([1319219,4,1,1197,8233,'414 w oak'])
+sales.append([1000000,2,1,672,6715,'218 w oak'])
+sales.append([1485000,2,1,2044,4967,'676 w palm'])
+sales.append([910000,3,1,1236,3954,'631 loma vista'])
+sales.append([1229000,3,2,1528,4923,'606 w mariposa'])
+sales.append([1275000,2,1,1194,7151,'521 concord'])
+sales.append([1400000,3,2,1564,6400,'423 loma vista'])
+sales.append([1603000,4,3,3555,7150,'423 virginia'])
+
+
 
 sales_data = []
 for home in sales:
@@ -35,10 +33,11 @@ for home in sales:
 
 sales_array = np.array(sales_data)
 sales_fit = linear_model.LinearRegression()
-sales_fit.fit(sales_array[:,1:5],sales_array[:,0])
+sales_fit.fit(sales_array[:,3:5],sales_array[:,0])
 
 print sales_fit.coef_
+print sales_fit.intercept_
 
-predict_inputs = [3,2,1000,6000]
+predict_inputs = [1000,6000]
 
 print sales_fit.predict(predict_inputs)
